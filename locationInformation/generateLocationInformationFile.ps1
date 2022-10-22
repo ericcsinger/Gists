c0d305ed9b375dc76992f6d06309ee9ca49a2533
Eric C. Singer <ericcsinger@users.noreply.github.com>
2022-10-22 15:21:00 -0400

    updated
    Changed some of the fields and made sure to include the country info as well.

--- locationInformation/generateLocationInformationFile.ps1
+++ locationInformation/generateLocationInformationFile.ps1
@@ -2,9 +2,11 @@
 #START: Import Data
 
 $ISO3166_1 = Import-Csv -Path ".\ISO3166_1.csv"
-$ISO3166_2 = Import-Csv -Path ".\loc221csv\2022-1 SubdivisionCodes.csv"
+$ISO3166_2 = Get-Content -Path ".\loc221csv\2022-1 SubdivisionCodes.csv" -Encoding oem | ConvertFrom-Csv
+$ISO3166_2 = Import-Csv -Path ".\loc221csv\2022-1 SubdivisionCodes.csv"  
 $AllCapitals = Import-CSV -Path ".\ip2location-country-information-basic\IP2LOCATION-COUNTRY-INFORMATION-BASIC.CSV"
 $AllCountryInformation = Import-Csv -Path ".\ip2location-geonameid\IP2LOCATION-GEONAMEID.CSV"
+
 
 
 #END: Import Data
@@ -29,12 +31,20 @@ $AllLocationInformationJoined = Foreach ($Country in $ISO3166_1)
         'iso3166-1_name' = $Country.name
         'iso3166-1_numeric' = $Country.numeric
         capital = $($Capital.capital)
+        allCountryInformation = Foreach ($CountryInfo in $CountryInformation)
+            {
+            [pscustomobject][ordered]@{
+                regionName = $($CountryInfo.Region_Name).Trim()
+                cityName = $($CountryInfo.City_Name).Trim()
+                }
+            }
+
         allSubdivisions = Foreach ($sub in $Subdivisions)
             {
             [pscustomobject][ordered]@{
-                'iso3166-2_code' = "$($sub.'iso3166-1_alpha2')-$($sub.'iso3166-2_code')".ToLower()
-                'iso3166-2_name' = $($sub.'iso3166-2_name')
-                'iso3166-2_type' = $($sub.'iso3166-2_type')
+                'iso3166-2_code' = "$($sub.'iso3166-1_alpha2')-$($sub.'iso3166-2_code')".ToLower().Trim()
+                'iso3166-2_name' = $($sub.'iso3166-2_name').Trim()
+                'iso3166-2_type' = $($sub.'iso3166-2_type').Trim()
                 }
             }
         }
@@ -58,6 +68,7 @@ Foreach ($Location in $AllLocationInformationJoined)
         'iso3166-1_numeric' = $Location.'iso3166-1_numeric'
         capital = $($Location.capital)
         allSubdivisions = [ordered]@{}
+        allCountryInformation = [ordered]@{}
         }
 
     Foreach ($Sub in $Location.allSubdivisions)
@@ -68,8 +79,25 @@ Foreach ($Location in $AllLocationInformationJoined)
             'iso3166-2_name' = $Sub.'iso3166-2_name'
             'iso3166-2_type' = $Sub.'iso3166-2_type'
             }
+        if ($LocationObject.allSubdivisions.Keys -contains $Sub.'iso3166-2_code')
+            {
+            $LocationObject.allSubdivisions.$($Sub.'iso3166-2_code') = $SubObject
+            }
+        else 
+            {
+            $LocationObject.allSubdivisions.Add($Sub.'iso3166-2_code', $SubObject) 
+            }
+        
+        }
 
-        $LocationObject.allSubdivisions.Add($Sub.'iso3166-2_code', $SubObject) 
+    Foreach ($CountryInfo in $Location.allCountryInformation)
+        {
+        $CountryInfoObject = $null
+        $CountryInfoObject = [ordered]@{
+            'iso3166-2_name' = $CountryInfoObject.regionName
+            'iso3166-2_type' = $CountryInfoObject.cityName
+            }
+        $LocationObject.allSubdivisions.Add("$($CountryInfo.regionName)_$($CountryInfo.CityName)", $CountryInfoObject) 
         }
 
     $AllLocationInformationJoinedHashTable.add($Location.'iso3166-1_alpha3', $LocationObject)
